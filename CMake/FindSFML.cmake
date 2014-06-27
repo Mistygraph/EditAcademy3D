e46afcdece488ee51502e9bbaa7947ba89852647
Mistygraph <jlyhyanni@hotmail.com>
2014-06-26 23:51:23 -0700

    find sfml

--- CMake/FindSFML.cmake
+++ CMake/FindSFML.cmake
@@ -5,28 +5,40 @@
 #  SFML_LIBRARS - The libraries needed to use SFML
 #  SFML_DEFINITIONS - Compiler switches required for using SFML
 
+#set(sfml_lib Graphics Audio Network System Window) #OpenGL)
+set(sfml_lib GRAPHICS AUDIO NETWORK SYSTEM WINDOW)
+set(header_path /usr/local/include)
+set(lib_path /usr/local/lib)
 
-find_path(SFML_INCLUDE_DIRS 
-  NAMES SFML/Graphics.hpp #SFML/Audio.hpp SFML/Network.hpp SFML/System.hpp SFML/Window.hpp SFML/OpenGL.hpp
-  PATH /usr/local/include
-  DOC "The SFML include directory")
+foreach(name ${sfml_lib})
+  #header
+  find_path(SFML_${name}_INCLUDE_DIRS 
+    NAMES SFML/${name}
+    PATH ${header_path}
+    DOC "The SFML ${name} include directory")
 
-find_library(SFML_LIBRARIES 
-  NAMES sfml-graphics # sfml-audio sfml-network sfml-system sfml-window
-  PATHS /usr/local/lib
-  DOC "The SFML library")
+  #lib
+  #string(TOLOWER ${name} lname)
+  find_library(SFML_${name}_LIBRARIES 
+    NAMES sfml-${name}
+    PATHS ${lib_path}
+    DOC "The SFML ${name} library")
 
+  string(TOUPPER ${name} uname)
+  include(FindPackageHandleStandardArgs)
+  # handle the QUIETLY and REQUIRED arguments and set LOGGING_FOUND to TRUE
+  # if all listed variables are TRUE
+  find_package_handle_standard_args(SFMLS DEFAULT_MSG SFML_${name}_INCLUDE_DIRS SFML_${name}_LIBRARIES)
+  
+  if (SFML_FOUND)
+    set(SFML_LIBRARIES ${SFML_LIBRARIES} ${SFML_${name}_LIBRARIES})
+    set(SFML_INCLUDE_DIRS ${SFML_INCLUDE_DIRS} ${SFML_${name}_INCLUDE_DIRS} )
+    set(SFML_DEFINITIONS)
+  endif()
 
-include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set LOGGING_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(SFML DEFAULT_MSG SFML_INCLUDE_DIRS SFML_LIBRARIES)
+endforeach()
 
-if (SFML_FOUND)
-  set(SFML_LIBRARIES ${SFML_LIBRARIES} )
-  set(SFML_INCLUDE_DIRS ${SFML_INCLUDE_DIRS} )
-  set(SFML_DEFINITIONS)
-endif()
+  find_package_handle_standard_args(SFMLS DEFAULT_MSG SFML_INCLUDE_DIRS SFML_LIBRARIES)
 
 # Tell cmake GUIs to ignore the "local" variables.
 mark_as_advanced(SFML_INCLUDE_DIRS SFML_LIBRARIES)
